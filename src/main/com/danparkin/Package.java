47f8270e655f1d5fcfa4c4f4be184aa2e7ce164e
Daniel Parkin <danielparkin1@gmail.com>
2018-07-11 12:56:12 +0100

    create static factory method on SizedPackage

--- src/main/com/danparkin/Package.java
+++ src/main/com/danparkin/Package.java
@@ -6,32 +6,12 @@ public class Package {
     private final SizedPackage sizedPackage;
 
     public Package(int weight, int height, int width, int depth) {
-        sizedPackage = createSizedPackage(depth, height, weight, width);
+        sizedPackage = SizedPackage.createSizedPackage(depth, height, weight, width);
     }
 
     public BigDecimal PostageInBaseCurrency()
     {
         return sizedPackage.postageInBaseCurrency();
-    }
-
-    private static SizedPackage createSizedPackage(int depth, int height, int weight, int width) {
-        if (isSmall(depth, height, weight, width))
-        {
-            return new SmallPackage();
-        }
-        if (isMedium(depth, height, weight, width))
-        {
-            return new MediumPackage(weight);
-        }
-        return new LargePackage(depth, height, weight, width);
-    }
-
-    private static boolean isMedium(int depth, int height, int weight, int width) {
-        return weight <= 500 && height <= 324 && width <= 229 && depth <= 100;
-    }
-
-    private static boolean isSmall(int depth, int height, int weight, int width) {
-        return weight <= 60 && height <= 229 && width <= 162 && depth <= 25;
     }
 
 }
