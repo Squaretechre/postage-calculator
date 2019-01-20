9cbb501f65e4fbae392bcffb5972a62277efde2c
Daniel Parkin <danielparkin1@gmail.com>
2019-01-20 20:15:06 +0000

    inline getters

--- src/main/src/LargePackage.java
+++ src/main/src/LargePackage.java
@@ -2,13 +2,13 @@ package src;
 
 import java.math.BigDecimal;
 
-public class LargePackage {
+class LargePackage {
     private final int depth;
     private final int height;
     private final int weight;
     private final int width;
 
-    public LargePackage(int depth, int height, int weight, int width) {
+    LargePackage(int depth, int height, int weight, int width) {
         this.depth = depth;
         this.height = height;
         this.weight = weight;
@@ -16,22 +16,6 @@ public class LargePackage {
     }
 
     BigDecimal largePackagePostageInBaseCurrency() {
-        return new BigDecimal(Math.max(getWeight(), getHeight() * getWidth() * getDepth() / 1000) * 6);
-    }
-
-    public int getDepth() {
-        return depth;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public int getWidth() {
-        return width;
+        return new BigDecimal(Math.max(weight, height * width * depth / 1000) * 6);
     }
 }
