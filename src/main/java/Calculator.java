195f8a86c30b001cfa89cad3c056d1a003f0e46f
Daniel Parkin <danielparkin1@gmail.com>
2019-01-20 21:22:39 +0000

    apply code formatting

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,36 +1,31 @@
 import java.math.BigDecimal;
 
 class Calculator {
-    Money Calculate(int weight, int height, int width, int depth, Currency currency)
-    {
+    Money Calculate(int weight, int height, int width, int depth, Currency currency) {
         var postageInBaseCurrency = PostageInBaseCurrency(weight, height, width, depth);
         return ConvertCurrency(postageInBaseCurrency, currency);
     }
 
-    private BigDecimal PostageInBaseCurrency(int weight, int height, int width, int depth)
-    {
-        if (weight <= 60 && height <= 229 && width <= 162 && depth <= 25)
-        {
+    private BigDecimal PostageInBaseCurrency(int weight, int height, int width, int depth) {
+        if (weight <= 60 && height <= 229 && width <= 162 && depth <= 25) {
             return new BigDecimal(120);
         }
-        if (weight <= 500 && height <= 324 && width <= 229 && depth <= 100)
-        {
+        if (weight <= 500 && height <= 324 && width <= 229 && depth <= 100) {
             return new BigDecimal(weight * 4);
         }
-        return new BigDecimal(Math.max(weight, height*width*depth/1000)*6);
+        return new BigDecimal(Math.max(weight, height * width * depth / 1000) * 6);
     }
 
-    private Money ConvertCurrency(BigDecimal amountInBaseCurrency, Currency currency)
-    {
+    private Money ConvertCurrency(BigDecimal amountInBaseCurrency, Currency currency) {
         if (currency == Currency.Gbp)
             return new Money(Currency.Gbp, amountInBaseCurrency);
-        if(currency == Currency.Eur) {
+        if (currency == Currency.Eur) {
             var commission = new BigDecimal(200);
             var basePrice = amountInBaseCurrency.add(commission);
             var rate = new BigDecimal(1.25);
             return new Money(Currency.Eur, basePrice.multiply(rate));
         }
-        if(currency == Currency.Chf) {
+        if (currency == Currency.Chf) {
             var commission = new BigDecimal(200);
             var basePrice = amountInBaseCurrency.add(commission);
             var rate = new BigDecimal(1.36);
--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -6,8 +6,7 @@ public class Money {
     private final Currency currency;
     private final BigDecimal amount;
 
-    Money(Currency currency, BigDecimal amount)
-    {
+    Money(Currency currency, BigDecimal amount) {
         this.currency = currency;
         this.amount = amount;
     }
